f478ef60f40a5d1b3350eaaf9dd4b0823a48358a
Sarah Preston <seprest@gmail.com>
2022-09-16 19:51:34 -0500

    created GET route client and server side, and tested.

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -3,6 +3,20 @@ const router = express.Router();
 const pool = require('../modules/pool');
 
 //GET ROUTE
+router.get('/', (req, res) => {
+    console.log('GET /gallery received a request');
+    let queryText = 
+        `SELECT * FROM "gallery"
+            ORDER BY id;`;
+    pool.query(queryText)
+        .then(result => {
+            res.send(result.rows);
+        })
+        .catch(error => {
+            console.log('error getting gallery', error);
+            res.sendStatus(500);
+        });
+});
 
 //POST ROUTE
 
